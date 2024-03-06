d3b4d56ff8e8ec241494c97ee18a50aa23b825fa
brylanbrystophe <32617715-brylanbrystophe@users.noreply.replit.com>
2024-03-06 14:26:53 +0000

    Reindent

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,12 @@
 // import org.junit.jupiter.api.Test;
 
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Java jest wolna xD :(");
-  }
-  
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
+	public static void main(String[] args) {
+		System.out.println("Java jest wolna xD :(");
+	}
+	
+	// @Test
+	// void addition() {
+	//     assertEquals(2, 1 + 1);
+	// }
 }
